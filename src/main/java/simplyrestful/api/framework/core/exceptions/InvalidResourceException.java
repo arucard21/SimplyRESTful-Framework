6cbc8741358fdbe963afa65fd818f2417489a31f
arucard21 <arucard21@gmail.com>
2018-03-12 10:54:59 +0100

    Add generated serialVersionUID to custom exceptions

--- src/main/java/simplyrestful/api/framework/core/exceptions/InvalidResourceException.java
+++ src/main/java/simplyrestful/api/framework/core/exceptions/InvalidResourceException.java
@@ -1,6 +1,11 @@
 package simplyrestful.api.framework.core.exceptions;
 
 public class InvalidResourceException extends Exception{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -4170046634966114763L;
+
 	public InvalidResourceException(String errorMessage){
 		super(errorMessage);
 	}
--- src/main/java/simplyrestful/api/framework/core/exceptions/InvalidSelfLinkException.java
+++ src/main/java/simplyrestful/api/framework/core/exceptions/InvalidSelfLinkException.java
@@ -1,6 +1,11 @@
 package simplyrestful.api.framework.core.exceptions;
 
 public class InvalidSelfLinkException extends InvalidResourceException{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -4492326488567095868L;
+
 	public InvalidSelfLinkException(String errorMessage){
 		super(errorMessage);
 	}
