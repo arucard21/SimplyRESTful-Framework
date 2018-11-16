5b39ecdc26fa9ed09abc94bc6f4d80262d65dcb9
Riaas Mokiem <riaasm@topdesk.com>
2018-11-16 12:43:25 +0100

    Fix incorrect import

--- SimplyRESTful-jersey-spring-boot/src/main/java/simplyrestful/springboot/configuration/jersey/JerseySpringBootConfiguration.java
+++ SimplyRESTful-jersey-spring-boot/src/main/java/simplyrestful/springboot/configuration/jersey/JerseySpringBootConfiguration.java
@@ -6,16 +6,14 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
+import simplyrestful.api.framework.core.hal.HALJacksonJsonProvider;
 import simplyrestful.api.framework.core.servicedocument.WebResourceRoot;
-import simplyrestful.springboot.CustomRequestPropertiesFilter;
-import simplyrestful.springboot.HALJacksonJsonProvider;
 
 @Configuration
 public class JerseySpringBootConfiguration implements ResourceConfigCustomizer{
 	@Override
 	public void customize(ResourceConfig config) {
 		config.register(WebResourceRoot.class);
-		config.register(CustomRequestPropertiesFilter.class);
 		config.register(HALJacksonJsonProvider.class);
 		config.register(ApiListingResource.class);
 		config.register(SwaggerSerializers.class);
