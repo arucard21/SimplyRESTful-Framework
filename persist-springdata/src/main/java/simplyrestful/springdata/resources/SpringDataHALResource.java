87232e4ee4abdcc1582ef4fea5ec24640e640bd2
Riaas Mokiem <riaasm@topdesk.com>
2019-09-30 13:08:30 +0200

    Fix incorrect annotations on Spring Data HAL resoruce

--- persist-springdata/src/main/java/simplyrestful/springdata/resources/SpringDataHALResource.java
+++ persist-springdata/src/main/java/simplyrestful/springdata/resources/SpringDataHALResource.java
@@ -2,8 +2,6 @@ package simplyrestful.springdata.resources;
 
 import java.util.UUID;
 
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 
@@ -12,7 +10,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import simplyrestful.api.framework.resources.HALResource;
 
 @MappedSuperclass
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class SpringDataHALResource extends HALResource{
 	@JsonIgnore
 	@NotNull
