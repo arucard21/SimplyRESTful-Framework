1e9dc379607c385066e1d3f02bafa17c10e8b56d
Riaas Mokiem <riaasm@topdesk.com>
2019-12-06 13:40:19 +0100

    Remove unnecessary test code (and a small code improvement)

--- SimplyRESTful/src/integrationTest/java/simplyrestful/api/framework/core/WebResourceIntegrationTest.java
+++ SimplyRESTful/src/integrationTest/java/simplyrestful/api/framework/core/WebResourceIntegrationTest.java
@@ -8,19 +8,15 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response.Status.Family;
-import javax.ws.rs.core.UriInfo;
 
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.client.WebClient;
-import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
@@ -37,23 +33,17 @@ public class WebResourceIntegrationTest{
 	private static final String QUERY_PARAM_COMPACT = "compact";
 	private Server server;
 	private WebClient client;
-	@Mock
-	private UriInfo uriInfo;
-	@InjectMocks
-	private TestWebResource webResource;
-	
+
 	@BeforeEach
 	private void createServerAndClient() {
 		startServer();
 		createClient();
 	}
-	
+
 	private void startServer() {
-		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();	   
+		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
 		sf.setResourceClasses(TestWebResource.class, WebResourceRoot.class);
 		sf.setProvider(new JacksonJsonProvider(new HALMapper()));
-		sf.setResourceProvider(TestWebResource.class, new SingletonResourceProvider(webResource, true));
-		sf.setResourceProvider(WebResourceRoot.class, new SingletonResourceProvider(new WebResourceRoot(), true));
 		sf.setAddress(TestResource.TEST_REQUEST_BASE_URI.toString());
 		server = sf.create();
 	}
@@ -72,29 +62,29 @@ public class WebResourceIntegrationTest{
 
 	@Test
 	public void webResource_shouldReturnAllResources_whenGETReceivedOnWebResourcePath(){
-		HALCollection<TestResource> collection = (HALCollection<TestResource>) client
+		HALCollection<TestResource> collection = client
 				.path(WEB_RESOURCE_PATH)
 				.get(new GenericType<HALCollection<TestResource>>() {});
 		Assertions.assertEquals(2, collection.getTotal());
 		Assertions.assertTrue(collection.getItem().contains(TestWebResource.TEST_RESOURCE.getSelf()));
 	}
-	
+
 	@Test
 	public void webResource_shouldReturnAllResourcesEmbedded_whenGETReceivedOnWebResourcePathAndCompactIsFalse(){
-		HALCollection<TestResource> collection = (HALCollection<TestResource>) client
+		HALCollection<TestResource> collection = client
 				.path(WEB_RESOURCE_PATH)
-				.query(QUERY_PARAM_COMPACT, new Boolean(false).toString())
+				.query(QUERY_PARAM_COMPACT, Boolean.toString(false))
 				.get(new GenericType<HALCollection<TestResource>>() {});
 		Assertions.assertEquals(2, collection.getTotal());
 		Assertions.assertTrue(collection.getItemEmbedded().contains(TestWebResource.TEST_RESOURCE));
 	}
-	
+
 	@Test
 	public void webResource_shouldReturnSingleResource_whenGETReceivedWithID(){
 		TestResource testResource = client.path(WEB_RESOURCE_PATH).path(TestResource.TEST_RESOURCE_ID).get(TestResource.class);
 		Assertions.assertEquals(TestWebResource.TEST_RESOURCE, testResource);
 	}
-	
+
 	@Test
 	public void webResource_shouldCreateResourceAndReturnLocationURI_whenPOSTReceivedWithNewResource(){
 		TestResource expectedResource = TestResource.random();
