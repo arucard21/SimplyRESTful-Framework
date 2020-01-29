6458759c90b860f61688759aaecc10c3ad1e7469
arucard21 <arucard21@gmail.com>
2020-01-29 13:13:53 +0100

    Add tests for create() and exists() methods

--- client/src/integrationTest/java/simplyrestful/api/framework/client/SimplyRESTfulClientTest.java
+++ client/src/integrationTest/java/simplyrestful/api/framework/client/SimplyRESTfulClientTest.java
@@ -3,6 +3,9 @@ package simplyrestful.api.framework.client;
 import java.util.List;
 import java.util.UUID;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.UriBuilder;
+
 import org.apache.cxf.endpoint.Server;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
@@ -17,6 +20,7 @@ import simplyrestful.jetty.deploy.ServerBuilder;
 
 @ExtendWith(MockitoExtension.class)
 public class SimplyRESTfulClientTest {
+    private static final UUID UUID_NIL = UUID.fromString("00000000-0000-0000-0000-000000000000");
     private static Server server;
     private static SimplyRESTfulClient<TestResource> client;
 
@@ -44,22 +48,69 @@ public class SimplyRESTfulClientTest {
     }
 
     @Test
-    public void client_shouldReturnTestResourceIdentifiers() {
+    public void listResourceIds_shouldReturnTestResourceIdentifiers() {
 	List<UUID> listOfResourceIdentifiers = client.listResourceIdentifiers(-1, -1);
 	Assertions.assertEquals(2, listOfResourceIdentifiers.size());
 	Assertions.assertTrue(listOfResourceIdentifiers.contains(TestResource.TEST_RESOURCE_ID));
     }
     
     @Test
-    public void client_shouldReturnTestResources() {
+    public void listResources_shouldReturnTestResources() {
 	List<TestResource> listOfResourceIdentifiers = client.listResources(-1, -1);
 	Assertions.assertEquals(2, listOfResourceIdentifiers.size());
 	Assertions.assertTrue(listOfResourceIdentifiers.contains(TestWebResource.TEST_RESOURCE));
     }
 
     @Test
-    public void client_shouldReturnTestResource() {
+    public void read_shouldReturnTestResource() {
 	TestResource actual = client.read(TestResource.TEST_RESOURCE_ID);
 	Assertions.assertEquals(TestWebResource.TEST_RESOURCE, actual);
+    }
+    
+    @Test
+    public void read_shouldReturnTestResource_whenAnNonExistingIdIsUsed() {
+	Assertions.assertThrows(NotFoundException.class, () -> client.read(UUID_NIL));
+    }
+    
+    @Test
+    public void read_shouldThrowNullPointerException_whenProvidingNullAsArgument() {
+	Assertions.assertThrows(NullPointerException.class, () -> client.read(null));
+    }
+    
+    @Test
+    public void create_shouldReturnTheIdOfTheCreatedTestResource() {
+	UUID actual = client.create(new TestResource());
+	Assertions.assertNotEquals(TestResource.TEST_RESOURCE_ID, actual);
+    }
+
+    @Test
+    public void create_shouldReturnTheSameId_whenTheProvidedTestResourceContainsAnId() {
+	UUID actual = client.create(TestResource.withId(UUID_NIL));
+	Assertions.assertEquals(UUID_NIL, actual);
+    }
+    
+    @Test
+    public void create_shouldThrowNullPointerException_whenProvidingNullAsArgument() {
+	Assertions.assertThrows(NullPointerException.class, () -> client.create(null));
+    }
+    
+    @Test
+    public void exists_shouldReturnTrue_whenProvidingAnExistingIdAsArgument() {
+	Assertions.assertTrue(client.exists(TestResource.TEST_RESOURCE_ID));
+    }
+    
+    @Test
+    public void exists_shouldReturnFalse_whenProvidingAnNonExistingIdAsArgument() {
+	Assertions.assertFalse(client.exists(UUID_NIL));
+    }
+    
+    @Test
+    public void exists_shouldReturnTrue_whenProvidingAnExistingResourceUriAsArgument() {
+	Assertions.assertTrue(client.exists(TestResource.TEST_RESOURCE_URI));
+    }
+    
+    @Test
+    public void exists_shouldReturnFalse_whenProvidingAnNonExistingResourceUriAsArgument() {
+	Assertions.assertFalse(client.exists(UriBuilder.fromUri(TestWebResource.TEST_REQUEST_BASE_URI).path(UUID_NIL.toString()).build()));
     }
 }
