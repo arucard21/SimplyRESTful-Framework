cc988a83650a46ec5202bed8cdd9dfba54d21d39
arucard21 <arucard21@gmail.com>
2020-10-22 07:13:34 +0200

    Allow overriding the request URI using an HTTP header.

--- deploy-springboot-jersey/src/main/java/simplyrestful/springboot/configuration/jersey/JerseySpringBootConfiguration.java
+++ deploy-springboot-jersey/src/main/java/simplyrestful/springboot/configuration/jersey/JerseySpringBootConfiguration.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import simplyrestful.api.framework.core.filters.JsonFieldsFilter;
+import simplyrestful.api.framework.core.filters.UriCustomizer;
 import simplyrestful.api.framework.core.providers.JacksonHALJsonProvider;
 import simplyrestful.api.framework.core.providers.ObjectMapperProvider;
 import simplyrestful.api.framework.core.servicedocument.WebResourceRoot;
@@ -25,6 +26,7 @@ public class JerseySpringBootConfiguration implements ResourceConfigCustomizer {
 	config.register(ObjectMapperProvider.class);
 	config.register(JacksonHALJsonProvider.class);
 	config.register(JacksonJsonProvider.class);
+	config.register(UriCustomizer.class);
 	config.register(JsonFieldsFilter.class);
 	config.register(OpenApiResource.class);
 	config.register(AcceptHeaderOpenApiResource.class);
