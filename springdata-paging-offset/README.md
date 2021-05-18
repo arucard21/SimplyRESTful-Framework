d42bb94634b2fcadad9017b2af0a858e868c2409
arucard21 <arucard21@gmail.com>
2021-05-18 15:46:48 +0200

    Add library that provides an offset-based paging implementation for Spring Data.

---
+++ springdata-paging-offset/README.md
@@ -0,0 +1,4 @@
+# Spring Data Offset-based Paging
+[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg?style=plastic)](https://www.gnu.org/licenses/lgpl-3.0)
+
+An implementation of the Spring Data `Pageable` interface that provides offset-based pagination.
---
+++ springdata-paging-offset/src/main/java/simplyrestful/api/framework/springdata/paging/OffsetBasedPageRequest.java
@@ -0,0 +1,63 @@
+package simplyrestful.api.framework.springdata.paging;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public class OffsetBasedPageRequest implements Pageable{
+    private final int pageOffset;
+    private final int pageSize;
+    private final Sort sort;
+
+    public OffsetBasedPageRequest(int pageOffset, int pageSize) {
+	this.pageOffset = pageOffset;
+	this.pageSize = pageSize;
+	this.sort = Sort.unsorted();
+    }
+
+    public OffsetBasedPageRequest(int pageOffset, int pageSize, Sort sort) {
+	this.pageOffset = pageOffset;
+	this.pageSize = pageSize;
+	this.sort = sort == null ? Sort.unsorted() : sort;
+    }
+
+    @Override
+    public int getPageNumber() {
+	return pageOffset / pageSize;
+    }
+
+    @Override
+    public int getPageSize() {
+	return pageSize;
+    }
+
+    @Override
+    public long getOffset() {
+	return pageOffset;
+    }
+
+    @Override
+    public Sort getSort() {
+	return sort;
+    }
+
+    @Override
+    public Pageable next() {
+	 return new OffsetBasedPageRequest(pageOffset + pageSize, pageSize, sort);
+    }
+
+    @Override
+    public Pageable previousOrFirst() {
+	return hasPrevious() ? new OffsetBasedPageRequest(pageOffset - pageSize, pageSize, sort) : first();
+    }
+
+    @Override
+    public Pageable first() {
+	return new OffsetBasedPageRequest(0, pageSize, sort);
+    }
+
+    @Override
+    public boolean hasPrevious() {
+	return pageOffset > pageSize;
+    }
+
+}
