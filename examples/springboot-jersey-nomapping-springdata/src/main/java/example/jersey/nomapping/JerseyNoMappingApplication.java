4792ebfd11c9c8537b11e7b5f69654e28508abf0
arucard21 <arucard21@gmail.com>
2021-09-05 20:44:09 +0200

    Remove direct configuration of JSON fields filter from example API, in favor of the now-provided auto-configuration.

--- examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
+++ examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
@@ -3,10 +3,6 @@ package example.jersey.nomapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import simplyrestful.api.framework.filters.JsonFieldsServletFilter;
 
 /**
  * Run the API server with the example endpoint and resource.
@@ -16,10 +12,5 @@ import simplyrestful.api.framework.filters.JsonFieldsServletFilter;
 public class JerseyNoMappingApplication {
     public static void main(String[] args) {
         SpringApplication.run(JerseyNoMappingApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean<JsonFieldsServletFilter> registerJsonFieldsFilter() {
-        return new FilterRegistrationBean<JsonFieldsServletFilter>(new JsonFieldsServletFilter());
     }
 }
