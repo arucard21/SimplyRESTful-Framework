b29281a31cc9c63022e717fac14d5226ee509ade
arucard21 <arucard21@gmail.com>
2022-10-14 10:46:20 +0200

    Add Swagger-UI to backend.

--- examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/ExampleResourceConfig.java
+++ examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/ExampleResourceConfig.java
@@ -1,6 +1,7 @@
 package example.jersey.nomapping;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.context.annotation.Configuration;
 
 import example.jersey.nomapping.resources.ExampleWebResource;
@@ -11,5 +12,6 @@ public class ExampleResourceConfig extends ResourceConfig {
     public ExampleResourceConfig() {
         register(ExampleWebResource.class);
         register(ObjectMapperProvider.class);
+        property(ServletProperties.FILTER_FORWARD_ON_404, true);
     }
 }
