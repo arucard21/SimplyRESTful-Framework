40536469b3d24624cda2b18d206bea64eebe0a2c
arucard21 <arucard21@gmail.com>
2022-10-14 10:46:44 +0200

    Define necessary OpenAPI Info and Servers sections.

--- examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
+++ examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
@@ -4,11 +4,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
+
 /**
  * Run the API server with the example endpoint and resource.
  */
 @SpringBootApplication
 @EntityScan("example.resources.jpa")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "test title",
+				description = "test description",
+				version = "1.0.0"
+		),
+		servers = {
+				@Server(
+						url = "http://{hostname}:{port number}",
+						variables = {
+								@ServerVariable(
+										name = "hostname",
+										defaultValue = "localhost"
+								),
+								@ServerVariable(
+										name = "port number",
+										defaultValue = "8888"
+								)
+
+						}
+				)
+		}
+)
 public class JerseyNoMappingApplication {
     public static void main(String[] args) {
         SpringApplication.run(JerseyNoMappingApplication.class, args);
