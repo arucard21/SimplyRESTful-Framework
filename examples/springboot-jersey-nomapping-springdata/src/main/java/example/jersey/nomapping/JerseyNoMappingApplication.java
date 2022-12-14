47fb9fc1f7365f004270922ccae2ec459dff1992
arucard21 <arucard21@gmail.com>
2022-12-14 14:29:06 +0100

    Don't define servers variable so Swagger UI will run the calls relative where it's hosted.

--- examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
+++ examples/springboot-jersey-nomapping-springdata/src/main/java/example/jersey/nomapping/JerseyNoMappingApplication.java
@@ -6,37 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.servers.ServerVariable;
 
 /**
  * Run the API server with the example endpoint and resource.
  */
 @SpringBootApplication
 @EntityScan("example.resources.jpa")
-@OpenAPIDefinition(
-		info = @Info(
-				title = "test title",
-				description = "test description",
-				version = "1.0.0"
-		),
-		servers = {
-				@Server(
-						url = "http://{hostname}:{port number}",
-						variables = {
-								@ServerVariable(
-										name = "hostname",
-										defaultValue = "localhost"
-								),
-								@ServerVariable(
-										name = "port number",
-										defaultValue = "8888"
-								)
-
-						}
-				)
-		}
-)
+@OpenAPIDefinition( info = @Info(title = "test title", description = "test description", version = "1.0.0"))
 public class JerseyNoMappingApplication {
     public static void main(String[] args) {
         SpringApplication.run(JerseyNoMappingApplication.class, args);
